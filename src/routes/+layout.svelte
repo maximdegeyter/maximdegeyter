a0c22efc286d20096c5581acb5128338c41bc786
Maxim De Geyter <maxim.degeyter@craftzing.com>
2023-01-23 08:20:02 +0100

    ✨ create homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,75 @@
+<script>
+  const name = 'Maxim De Geyter';
+</script>
+
+<header>
+  <div class="header__container">
+    <strong class="logo"><a href="/">Maxim De Geyter</a></strong>
+    <nav>
+      <a href="/about">About</a>
+      <a href="/works">Works</a>
+      <a href="/contact">Contact</a>
+    </nav>
+    <div />
+  </div>
+</header>
+<main>
+  <slot />
+</main>
+<footer>
+  <p>© 2023 Maxim De Geyter. All Rights Reserved.</p>
+</footer>
+
+<style lang="scss">
+  header {
+    height: 6rem;
+    border-bottom: 1px solid #e5e5e5;
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    backdrop-filter: blur(1rem);
+  }
+
+  .header__container {
+    width: 75rem;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    div {
+      width: 33%;
+    }
+
+    nav {
+      width: 33%;
+      a {
+        font-family: 'Helvetica Neue';
+        font-size: 1.125rem;
+        border: none;
+        padding: 1rem 1.5rem;
+        border-radius: 0.25rem;
+        color: #404040;
+
+        &:hover {
+          background-color: #f7f7f7;
+          color: black;
+        }
+      }
+    }
+  }
+
+  .logo {
+    width: 33%;
+    a {
+      font-size: 1.5rem;
+      color: black;
+      border: none;
+    }
+  }
+
+  footer {
+    height: 3rem;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // @ts-ignore
+  import Image from '../assets/maxim.jpg?w=400&h=300&webp';
+</script>
+
+<section class="hero__section">
+  <div>
+    <h1>Hi, I'm Maxim</h1>
+    <p class="hero__intro">
+      Welcome to my personal spot on the internet! I’m a front-end developer with a passion for
+      bringing concepts to life and constantly <strong>improving site interaction</strong> for
+      users. In my work, I place a strong <strong>emphasis on A11y and user experience</strong>,
+      ensuring that everyone can enjoy the content and features of the sites I build.
+    </p>
+    <a href="/">Get to know me</a>
+  </div>
+  <img src={Image} alt="A pic of me staring at the sky" width="333" height="444" />
+</section>
+
+<style lang="scss">
+  .hero__section {
+    margin-top: 12rem;
+    height: calc(100vh - 6rem);
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .hero__intro {
+    width: 45rem;
+  }
+
+  img {
+    border: 2px solid #333333;
+    border-radius: 0.5rem;
+    object-fit: cover;
+    position: relative;
+    --tw-shadow: 16px 16px 0px #ff6543;
+    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000),
+      var(--tw-shadow);
+  }
+</style>
